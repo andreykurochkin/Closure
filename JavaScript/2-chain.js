7f37c0146523eabe61f3e6a7939c5af30e36aa9e
krch <krch@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-10 10:42:03 +0300

    Modified chaining

--- JavaScript/2-chain.js
+++ JavaScript/2-chain.js
@@ -12,11 +12,31 @@ const hash = () => {
   return data;
 };
 
+const alterHash = () => {
+  const data = {};
+  Object.defineProperty(data, 'add', {
+    enumerable: false,
+    value: (key, value) => {
+      data[key] = value;
+      return data;
+    }
+  });
+  return data;
+};
+
 // Usage
 
 console.dir(
   hash()
     .add('name', 'Marcus')
     .add('city', 'Roma')
+    .add('born', 121)
+);
+
+console.dir(
+  alterHash()
+    .add('name', 'Marcus')
+    .add('city', 'Roma')
+    .add('born', 121)
     .add('born', 121)
 );
